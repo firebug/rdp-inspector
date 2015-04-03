a4343a17e08ffb9fb82eb597b4bc96d9df625114
Luca Greco <luca.greco@alcacoop.it>
2015-04-03 09:44:01 +0000

    add toolbar in the packet editor sidebar

--- data/inspector/components/packet-editor.js
+++ data/inspector/components/packet-editor.js
@@ -4,6 +4,7 @@ define(function(require, exports, module) {
 
   // Dependencies
   const React = require("react");
+  const ReactBootstrap = require("react-bootstrap");
 
   // Firebug SDK
   const { Reps } = require("reps/repository");
@@ -11,6 +12,44 @@ define(function(require, exports, module) {
 
   // Shortcuts
   const { DIV } = Reps.DOM;
+
+  const ButtonToolbar = React.createFactory(ReactBootstrap.ButtonToolbar);
+  const Button = React.createFactory(ReactBootstrap.Button);
+
+  /**
+   * TODO docs
+   */
+
+  var PacketEditorToolbar = React.createFactory(React.createClass({
+    displayName: "PacketEditorToolbar",
+
+    render: function() {
+      return ButtonToolbar({className: "toolbar"}, [
+        Button({ onClick: this.props.onSend, key: "send",
+                 bsStyle: "primary", bsSize: "xsmall",
+                 style: { marginLeft: 12, color: 'white' } }, "Send"),
+
+        Button({ onClick: this.props.onAddField, key: "add",
+                 bsStyle: "default", bsSize: "xsmall",
+                 style: { marginLeft: 12 } }, "Add Field"),
+
+        Button({ onClick: this.props.onRedo, key: "redo",
+                 className: "pull-right",
+                 disabled: !this.props.isRedoEnabled,
+                 bsStyle: "default", bsSize: "xsmall",
+                 style: { marginRight: 6 } }, "Redo"),
+        Button({ onClick: this.props.onUndo, key: "undo",
+                 className: "pull-right",
+                 disabled: !this.props.isUndoEnabled,
+                 bsStyle: "default", bsSize: "xsmall",
+                 style: { marginRight: 6 } }, "Undo"),
+        Button({ onClick: this.props.onClear, key: "clear",
+                 className: "pull-right",
+                 bsStyle: "danger", bsSize: "xsmall",
+                 style: { marginRight: 6, color: 'white' } }, "Clear")
+      ]);
+    }
+  }));
 
   /**
    * @template TODO
@@ -29,7 +68,8 @@ define(function(require, exports, module) {
       var selectedPacket = this.props.selectedPacket || {};
 
       return (
-        DIV({className: "editor"},
+        DIV({className: "details editor"},
+            PacketEditorToolbar({}),
             TreeView({key: "packet-editor", data: selectedPacket})
            )
       );
