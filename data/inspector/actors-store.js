786e1c0c262b2668abf975e7a7b1bc1413b156f6
Luca Greco <luca.greco@alcacoop.it>
2015-05-02 19:56:11 +0200

    collecting actorIDs in the actors store

--- data/inspector/actors-store.js
+++ data/inspector/actors-store.js
@@ -23,6 +23,12 @@ ActorsStore.prototype =
     var data = JSON.parse(event.data);
     this.actors.tab = data.tab;
     this.actors.global = data.global;
+    this.actorIDs = this.collectActorIDs([
+      data.tab.actorPool,
+      data.tab.extraPools,
+      data.global.actorPool,
+      data.global.extraPools
+    ], []);
 
     this.refreshActors();
   },
@@ -31,9 +37,32 @@ ActorsStore.prototype =
     this.actors = {};
   },
 
+  collectActorIDs: function(data, res) {
+    if (data instanceof Array) {
+      data.forEach((item) => {
+        this.collectActorIDs(item, res);
+      })
+    } else if (data instanceof Object) {
+      if (data.pool) {
+        this.collectActorIDs(data.pool, res);
+      } else if (data.actorID) {
+        // NOTE: remove the "serverNN." prefix from the actorID
+        var actorID = data.actorID.replace(/^server\d*\./, "");
+        if (res.indexOf(actorID) < 0) {
+          res.push(actorID);
+        }
+      }
+    }
+
+    return res;
+  },
+
   refreshActors: function() {
+    console.log("ACTOR IDS", this.actorIDs);
+
     this.app.setState({
-      actors: this.actors
+      actors: this.actors,
+      actorIDs: this.actorIDs
     })
   }
 }
