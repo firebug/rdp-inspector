ebb9d18217db7e9d57d9deb4bd0c89f42a9ad304
Luca Greco <luca.greco@alcacoop.it>
2015-05-15 20:56:38 +0200

    tweak indentation on karma spec files

--- karma-tests/components/main-tabbed-area-spec.js
+++ karma-tests/components/main-tabbed-area-spec.js
@@ -1,75 +1,75 @@
 /* eslint-env jasmine */
 
 define(function (require) {
-  "use strict";
+"use strict";
 
-  var React = require("react");
-  var { TestUtils } = React.addons;
+var React = require("react");
+var { TestUtils } = React.addons;
 
-  // Fake actions singleton
-  var actions = {};
+// Fake actions singleton
+var actions = {};
 
-  var { MainTabbedArea: mainTabbedArea } = require("components/main-tabbed-area");
-  var { PacketsSummaryComponent } = require("components/packets-summary");
-  var { PacketComponent } = require("components/packet");
-  var { PacketsPanelComponent } = require("components/packets-panel");
-  var { ActorsPanelComponent } = require("components/actors-panel");
+var { MainTabbedArea: mainTabbedArea } = require("components/main-tabbed-area");
+var { PacketsSummaryComponent } = require("components/packets-summary");
+var { PacketComponent } = require("components/packet");
+var { PacketsPanelComponent } = require("components/packets-panel");
+var { ActorsPanelComponent } = require("components/actors-panel");
 
-  var { PacketsStore } = require("packets-store");
+var { PacketsStore } = require("packets-store");
 
-  var theApp, packetsStore;
-  theApp = React.render(mainTabbedArea({ actions: actions }), document.body);
-  packetsStore = new PacketsStore(window, theApp);
+var theApp, packetsStore;
+theApp = React.render(mainTabbedArea({ actions: actions }), document.body);
+packetsStore = new PacketsStore(window, theApp);
 
-  describe("MainTabbedArea React Component", function() {
-    beforeAll(function () {
-      jasmine.addMatchers(require("karma-tests/custom-react-matchers"));
-    });
+describe("MainTabbedArea React Component", function() {
+  beforeAll(function () {
+    jasmine.addMatchers(require("karma-tests/custom-react-matchers"));
+  });
 
-    beforeEach(function () {
-      packetsStore.clear();
-    });
+  beforeEach(function () {
+    packetsStore.clear();
+  });
 
-    afterAll(function () {
-      React.unmountComponentAtNode(document.body);
-    });
+  afterAll(function () {
+    React.unmountComponentAtNode(document.body);
+  });
 
-    it("renders without errors", function() {
-      //console.log("theApp", theApp, theApp.getDOMNode());
-      expect(theApp).toBeDefined();
+  it("renders without errors", function() {
+    //console.log("theApp", theApp, theApp.getDOMNode());
+    expect(theApp).toBeDefined();
 
-      expect(theApp.getDOMNode()).toEqual(document.body.firstChild);
-    });
+    expect(theApp.getDOMNode()).toEqual(document.body.firstChild);
+  });
 
-    it("is composed by a PacketsPanel and an ActorsPanel", function () {
-      var components = [
-        PacketsPanelComponent,
-        ActorsPanelComponent
-      ];
+  it("is composed by a PacketsPanel and an ActorsPanel", function () {
+    var components = [
+      PacketsPanelComponent,
+      ActorsPanelComponent
+    ];
 
-      components.forEach((Component) => {
-        expect(Component).toBeFoundInReactTree(theApp, 1);
-      });
-    });
-
-    it("renders a PacketsSummary on PacketsStore.appendSummary", function () {
-      packetsStore.appendSummary();
-
-      expect(PacketsSummaryComponent).toBeFoundInReactTree(theApp, 1);
-    });
-
-    it("renders a Packet on PacketsStore.sendPacket", function () {
-      packetsStore.onSendPacket({
-        data: JSON.stringify({
-          to: "root",
-          type: "requestTypes"
-        })
-      });
-
-      // NOTE: packets-store refresh the app every 200 ms
-      packetsStore.refreshPackets(true);
-
-      expect(PacketComponent).toBeFoundInReactTree(theApp, 1);
+    components.forEach((Component) => {
+      expect(Component).toBeFoundInReactTree(theApp, 1);
     });
   });
+
+  it("renders a PacketsSummary on PacketsStore.appendSummary", function () {
+    packetsStore.appendSummary();
+
+    expect(PacketsSummaryComponent).toBeFoundInReactTree(theApp, 1);
+  });
+
+  it("renders a Packet on PacketsStore.sendPacket", function () {
+    packetsStore.onSendPacket({
+      data: JSON.stringify({
+        to: "root",
+        type: "requestTypes"
+      })
+    });
+
+    // NOTE: packets-store refresh the app every 200 ms
+    packetsStore.refreshPackets(true);
+
+    expect(PacketComponent).toBeFoundInReactTree(theApp, 1);
+  });
+});
 });
